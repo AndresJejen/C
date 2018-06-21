ba20f10892f7969be646a7ad9e55b3d70d69f131
Andres Jejen <gajcam@gmail.com>
2018-06-20 19:36:44 -0500

    Hola Mundo y Andres Jejen

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,6 +16,8 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola Mundo");
+        System.out.println("Soy Andres Jejen");
     }
     
 }
